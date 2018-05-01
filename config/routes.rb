9aa296e929e650a33189b2d5a391e9bfdfd565e6
Dimaon <kulikov.perm@gmail.com>
2018-05-01 13:16:24 +0300

    comments and subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   root "events#index"
   devise_for :users
-  resources :events
+  resources :events do
+  	resources :comments, only: [:create, :destroy]
+  	resources :subscriptions, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 
 end
