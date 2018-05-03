f4ef9f4b45bf48e50ad1e3d4fd44f9234707c94f
Dimaon <kulikov.perm@gmail.com>
2018-05-03 20:24:31 +0300

    Reafactoring validates: anonymous email and author of event cant subscribe

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,37 +3,31 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :event, presence: true
-  validate :uniq_email
-
-  unless 'user.present?'
-    validates :user_name, presence: true
-    validates :user_email, presence: true, format: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
-  end
-
+  validate :event_author_cant_subscribe
+  validates :user_name, presence: true unless 'user.present?'
   validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
-  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+  validates :user_email, presence: true, format: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ unless 'user.present?' || 'email_present_in_user?'
+  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?' || 'email_present_in_user?'
 
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    validate :email_present_in_user?
+    user.present? ? user.name : super
   end
 
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user.present? ? user.email : super
   end
 
   private
-  def uniq_email
-    if User.find_by(email: user_email).present?
+  def email_present_in_user?
+    if user.blank? && User.find_by(email: user_email).present?
       errors.add(:user_email, I18n.t('error.email_exist'))
     end
   end
-
+  # TODO
+  def event_author_cant_subscribe
+    if user == event.user
+      errors.add(:user_name, 'Нельзя подписать себя')
+    end
+  end
 end
